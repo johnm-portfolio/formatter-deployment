eeb80db10b6df4d7ebe93d360e9fc2fbbdf4ac11
John McNally <Jmcnally1@sheffield.ac.uk>
2026-06-17 20:13:11 +0100

    Functional and verified initial TOC logic

--- note_formatter/formatter.py
+++ note_formatter/formatter.py
@@ -5,10 +5,11 @@ import toc
 # print(len(load_symbol_config()["symbols"]))
 
 def format_text(text: str) -> str:
+    # Remove previous TOC
+    text = toc.remove_toc(text)
+    # Add new TOC
+    text = toc.add_toc(text)
     return text
-    # Add TOC (includes replacing previous one)
-    formatted_text = toc.add_toc(text)
-    return formatted_text
 
 def make_link(link: str, display: str) -> str:
     return f"[[{link}|{display}]]"
@@ -17,3 +18,4 @@ TEST_PATH = Path(r"C:\Users\johnk\Documents\D&D\Obsidian\PIRATE CAMPAIGN\TEST.md
 with open(TEST_PATH, 'r', encoding="utf-8") as f:
     contents = f.read()
 print(format_text(contents))
+with open(TEST_PATH, 'w', encoding="utf-8") as f: f.write(format_text(contents))
--- note_formatter/toc.py
+++ note_formatter/toc.py
@@ -1,27 +1,46 @@
-# from typing import Array
+# (A new line, then a horizontal rule on a new line, and a new line after it)
+HEADING_SEPARATOR = '\n<hr style="border-color:#52308c">\n\n' #extra \n before?
 
 def has_toc(text: str) -> bool:
-    HEADING_DEMARKATION = '<hr style="border-color:#52308c">'
-    return HEADING_DEMARKATION in text
+    return HEADING_SEPARATOR in text
 
 def extract_headings(text: str) -> list:
-    # Form [[headingLink, indLevel],...]
+    # A heading is in the format "# Some heading\n"
+    # Resulting list in the form [[headingLnk, headingLvl],...]
     headings = []
-    headings.append(["One", 1])
+
+    inCode = False # hashtags in code are not headings
+    for line in text.split("\n"):
+        if "```" in line: inCode = not inCode
+
+        if not inCode:
+            valid_heading = (len(line) > 2 # guard empty lines - at least "# ")
+                        and line[0] == "#" # very start is a hashtag
+                        and line.replace("#","")[0] == " ") # a space after any hashtags
+            if valid_heading:
+                heading_split = line.split(" ", 1)
+                heading_lvl = len(heading_split[0])
+                display_txt = heading_split[1]
+                headings.append([display_txt, heading_lvl])
     return headings
 
 def add_toc(text: str) -> str:
     # Get headings
     headings = extract_headings(text)
-    heading_text = "" + '\n\n<hr style="border-color:#52308c">\n'
-    # Remove previous TOC
-    text = remove_toc(text)
+    heading_text = ""
+    # Add each heading, including indentation level
+    for heading in headings:
+        heading_lvl = heading[1]
+        heading_text += ("   " * (heading_lvl - 1)) + f"- [[#{heading[0]}]]\n"
     # Insert new TOC before main file contents
-    text = heading_text + "\n" + text
+    heading_text += HEADING_SEPARATOR
+    text = heading_text + text
 
     return text
 
+# Remove EVERYTHING before the TOC delineation (should just be generated headings)
 def remove_toc(text: str) -> str:
-    return text
-
-# print(extract_headings(""))
+    if not has_toc(text):
+        return text
+    
+    return text.split(HEADING_SEPARATOR)[1]
