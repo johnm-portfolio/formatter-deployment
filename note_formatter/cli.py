09accb88b2ab4c49686492c3666b61e7c6cdf272
John McNally <Jmcnally1@sheffield.ac.uk>
2026-07-06 15:51:33 +0100

    Corrected bug, CLI run twice

--- note_formatter/cli.py
+++ note_formatter/cli.py
@@ -1,87 +1,109 @@
 from pathlib import Path
-import sys
 import argparse
+
 from note_formatter.formatter import format_text
 from note_formatter.config import load_config, update_prev_path
+
 
 def main():
     parser = argparse.ArgumentParser(
         prog="note-formatter",
-        description="Format markdown notes, intended for Obsidian"
+        description="Format markdown notes, intended for Obsidian",
     )
+
     parser.add_argument(
         "input",
         nargs="?",
-        help="(Markdown) file to process"
+        help="(Markdown) file to process",
     )
     parser.add_argument(
         "-o",
         "--output",
-        help="Separate output file path"
+        help="Separate output file path",
     )
     parser.add_argument(
         "-s",
         "--symbols",
-        help="Custom symbol configuration"
+        help="Custom symbol configuration",
     )
     parser.add_argument(
         "--no-toc",
         action="store_true",
-        help="Disable automatic table of contents generation"
+        help="Disable automatic table of contents generation",
     )
     parser.add_argument(
         "--inplace",
         action="store_true",
-        help="Format the file inplace (use the same input and output path)"
+        help="Format the file in place",
     )
+
     args = parser.parse_args()
 
-    inpt_path: Path | None = None
-    out_path: Path | None = None
-
     if args.input is None:
-        # Get user to enter the input path
-        inpt_path_str = input("Enter input file path (press Enter to use previous path): ").strip('"').replace("\\","/")
-        if inpt_path_str == "":
-            inpt_path = load_prev_path()
-        else:
-            inpt_path = Path(inpt_path_str)
-        # Get the user to enter the output path
-        if args.inplace:
-            out_path = inpt_path
-        else:
-            out_path_str = input("Enter output file path (press Enter to use the input path): ").strip('"').replace("\\","/")
-            if out_path_str == "":
-                out_path = inpt_path
-            else:
-                out_path = Path(out_path_str)
-                print(out_path)
-        #TODO Get the user to enter the symbols path
+        in_path, out_path = run_interactive(args)
     else:
-        inpt_path = Path(args.input.replace("\\","/"))
+        in_path, out_path = run_cli(args)
+
+    text = read_file(in_path)
+    formatted = format_text(text, not args.no_toc)
+
+    with open(out_path, "w", encoding="utf-8") as f:
+        f.write(formatted)
+
+    update_prev_path(in_path)
+
+
+def run_interactive(args) -> tuple[Path, Path]:
+    in_path_str = (
+        input("Enter input file path (press Enter to use previous path): ")
+        .strip('"')
+        .replace("\\", "/")
+    )
+
+    if in_path_str:
+        in_path = Path(in_path_str)
+    else:
+        in_path = load_prev_path()
 
     if args.inplace:
-        out_path = inpt_path
-    elif args.input is not None:
-        if args.output is None:
-            out_path = inpt_path
-        else:
-            out_path = Path(args.output.replace("\\","/"))
-    
-    inpt_file_content = read_file(inpt_path)
-    formatted_text = format_text(inpt_file_content, not args.no_toc)
+        return in_path, in_path
 
-    with open(out_path, 'w', encoding="utf-8") as f:
-        f.write(formatted_text)
+    out_path_str = (
+        input("Enter output file path (press Enter to use the input path): ")
+        .strip('"')
+        .replace("\\", "/")
+    )
 
-    update_prev_path(inpt_path)
+    if out_path_str:
+        out_path = Path(out_path_str)
+    else:
+        out_path = in_path
+
+    return in_path, out_path
+
+
+def run_cli(args) -> tuple[Path, Path]:
+    in_path = Path(args.input.replace("\\", "/"))
+
+    if args.inplace:
+        out_path = in_path
+    elif args.output:
+        out_path = Path(args.output.replace("\\", "/"))
+    else:
+        out_path = in_path
+
+    return in_path, out_path
+
 
 def load_prev_path() -> Path:
-    prev_path_str = load_config()["previous_path"]
-    return Path(prev_path_str.replace("\\","/"))
+    prev_path = load_config()["previous_path"]
+    return Path(prev_path.replace("\\", "/"))
+
 
 def read_file(path: Path) -> str:
-    with open(path, 'r', encoding="utf-8") as f:
+    with open(path, "r", encoding="utf-8") as f:
         return f.read()
 
-main()
+
+if __name__ == "__main__":
+    main()
